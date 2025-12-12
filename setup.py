0d6b05a932ff1828d8403e7a0de67dd1f6bd81d0
Robin Young <59581322+rbnyng@users.noreply.github.com>
2025-12-12 16:17:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='pingshui-rhyme',
-    version='0.19',
+    version='0.20',
     packages=setuptools.find_packages(),
     include_package_data=True,
     description='A Python package for classifying Chinese characters phonologically based on the Middle Chinese Pingshui rhyme scheme',
